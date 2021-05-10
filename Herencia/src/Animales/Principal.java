d4264cedb902fe2ba23c4d548529a3df608e313d
delgado29 <80289865+delgado29@users.noreply.github.com>
2021-05-10 17:25:25 -0600

    Arreglos

--- Herencia/src/Animales/Principal.java
+++ Herencia/src/Animales/Principal.java
@@ -1,14 +1,35 @@
 package Animales;
 
 import java.awt.*;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
 
+        System.out.println("Cuantos, registros quieres?");
+        int r= scanner.nextInt();
+        ;
+        for (int x=0; x<r; x++ ){
+            System.out.println("Inserta nombre:");
+            String n= scanner.next();
+
+
+        }
+
+        Perro[] arrayPerro=new Perro[2];
         Perro pluto=new Perro(Color.YELLOW, 30, "Galgo");
-
+        /*
         pluto.ladrar();
         pluto.comer();
-        pluto.moverse();
+        pluto.moverse();*/
+
+        arrayPerro[0]=pluto;
+        arrayPerro[1]=new Perro(Color.GRAY, 25, "Pitbull");
+
+        System.out.println( arrayPerro[0].getRaza() );
     }
+    
+
+
 }
